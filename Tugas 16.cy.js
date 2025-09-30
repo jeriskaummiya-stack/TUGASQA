2b9cd9b82e166dfed32adf3818408f3c73843427
Jerizka Ummiya <jeriskaummiya@gmail.com>
2025-09-30 21:55:27 +0700

    Add files via upload

---
+++ Tugas 16.cy.js
@@ -0,0 +1,16 @@
+describe('Login ke OrangeHRM ', () => {
+  it('Berhasil login dengan akun valid', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+    cy.get('input[name="username"]').type('Admin')
+    cy.get('input[name="password"]').type('admin123')
+    
+    cy.intercept('GET,https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/dashboard/employees/subunit').as('subunit')
+
+    cy.get('button[type="submit"]').click()
+
+    cy.wait('@subunit').its('respone.statuscode').should('eq' ,200)
+
+    cy.url().should('include', '/dashboard')
+    cy.contains('Dashboard').should('be.visible')
+     })
+})
