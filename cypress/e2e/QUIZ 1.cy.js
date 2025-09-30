2fe9fbe4c94906e9ea6b40eb10f08a4f6bcc51e7
Jerizka Ummiya <jeriskaummiya@gmail.com>
2025-09-30 19:27:43 +0700

    Update QUIZ 1.cy.js

--- cypress/e2e/QUIZ 1.cy.js
+++ cypress/e2e/QUIZ 1.cy.js
@@ -1,10 +1,10 @@
 describe('Login ke OrangeHRM ', () => {
   it('Berhasil login dengan akun valid', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/')
-    cy.get('input[name="username"]').type('Admin')
-    cy.get('input[name="password"]').type('admin123')
-    cy.get('button[type="submit"]').click()
-    cy.url().should('include', '/dashboard')
-    cy.contains('Dashboard').should('be.visible')
-     })
-})
+    cy.visit('https://opensource-demo.orangehrmlive.com/');
+    cy.get('[name="username"]').type('Admin');
+    cy.get('[name="password"]').type('admin123');
+    cy.get('[type="submit"]').click();
+    cy.url().should('include', '/dashboard');
+    cy.contains('Dashboard').should('be.visible');
+     });
+});
