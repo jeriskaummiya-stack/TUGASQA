2e38e03e99cb123337cbf9d3d04f8741236d0ea8
Jerizka Ummiya <jeriskaummiya@gmail.com>
2025-10-05 16:10:47 +0700

    Add files via upload

--- projek akhir/projek akhir forget pw.cy.js
+++ projek akhir/projek akhir forget pw.cy.js
@@ -1,7 +1,7 @@
 describe('Login ke OrangeHRM ', () => {
   it('login denga lupa password', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/');
-    cy.get('input[name="username"]').type('Admin');
+    cy.get('[name="username"]').type('Admin');
     
     cy.intercept('GET','https://opensource-demo.orangehrmlive.com/web/index.php/core/i18n/messages').as('messages');
 
@@ -10,5 +10,10 @@ describe('Login ke OrangeHRM ', () => {
     cy.wait('@messages').its('response.statusCode').should('eq',304);
 
     cy.get('.oxd-text.oxd-text--h6.orangehrm-forgot-password-title').should('contain','Reset Password');
-     });
+    cy.get('input[name="username"]').type('Admin');
+    cy.intercept('GET','https://opensource-demo.orangehrmlive.com/web/index.php/core/i18n/messages').as('messages');
+    cy.get('[type="submit"]').click();
+    cy.wait('@messages').its('response.statusCode').should('eq',304);
+    cy.get('.oxd-text.oxd-text--h6.orangehrm-forgot-password-title').should('contain', 'Reset Password link sent successfully');
+  });
 });
